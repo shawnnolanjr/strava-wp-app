f0900a6b1f704881fe4d8cc0249827b14a17e6d0
Shawn Nolan <shawn@shawnnolan.com>
2015-09-10 20:36:15 -0700

    - Adding start of strava plugin file.

---
+++ wp-strava-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WP Strava Plugin
+Plugin URI: http://wellrootedmedia.com/plugins/wp-strava-plugin
+Description: This plugin is to read data for users and display it on their website.
+Author: Well Rooted Media
+Version: 0.0.1
+Author URI: http://wellrootedmedia.com
+*/
